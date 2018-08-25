22a1feae68bc6c75a313bd58829b9bbfd23876fe
giacomociti <giacomociti@gmail.com>
2018-08-25 15:43:16 +0200

    Improve reference handling and add serialization test

--- tests/AvroTypeProvider.Tests/Tests.fs
+++ tests/AvroTypeProvider.Tests/Tests.fs
@@ -1,8 +1,33 @@
 module Tests
 
-open System
 open Xunit
+open Avro
+
+type T = AvroProvider<"""
+{
+    "type": "record",
+    "name": "author",
+    "fields": [
+        {"name": "name", "type": "string"},
+        {"name": "born", "type": "int"} ]
+}
+""">
+
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``serialization of simple record rountrips`` () =
+    let f = T.Factory()
+    let a = f.author(name="Joe", born = 2000)
+    T.Write("test.avro", [a])
+    let a' = T.Read("test.avro") |> Seq.exactlyOne
+    Assert.Equal(a.name, a'.name)
+    Assert.Equal(a.born, a'.born)
+
+
+
+
+
+
+
+
+
